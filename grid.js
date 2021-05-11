024089a949307555b0f4bb7503949b9e4737385b
Akanshu Kumar Singh <singhakanshu7@gmail.com>
2021-05-11 22:49:27 +0530

    menu container in progress

--- grid.js
+++ grid.js
@@ -1,6 +1,9 @@
 let leftCol = document.querySelector(".left_col");
 let topRow = document.querySelector(".top_row");
 let grid = document.querySelector(".grid");
+let addressInput = document.querySelector(".address-input");
+let bold = document.querySelector(".bold");
+
 let rows = 100;
 let cols = 26;
 
@@ -23,16 +26,43 @@ for (let i = 0; i < rows; i++) {
     row.setAttribute("class","row");
     for (let j = 0; j < cols; j++) {
             let cell = document.createElement("div");
-            cell.innerText = `${String.fromCharCode(65+j)} ${i+1}`;
+//            cell.innerText = `${String.fromCharCode(65+j)} ${i+1}`;
             cell.setAttribute("class","cell");
             cell.setAttribute("rid",i);
             cell.setAttribute("cid",j);
-//            cell.addEventListener("click",cellFxn);
+            cell.setAttribute("contenteditable","true");
             row.appendChild(cell);
     }
     grid.appendChild(row);
 }
 
-function cellFxn(e){
-    let 
+let allCells = document.querySelectorAll(".grid .cell");
+for (let i = 0; i < allCells.length; i++) {
+    allCells[i].addEventListener("click",function () {
+        let rid = allCells[i].getAttribute("rid");
+        let cid = allCells[i].getAttribute("cid");
+        rid = Number(rid);
+        cid = Number(cid);
+        let address = `${String.fromCharCode(65+cid)}${rid+1}`;
+        addressInput.value = address;
+    })
+}
+
+allCells[0].click();
+
+bold.addEventListener("click",function () {
+    let address = addressInput.value;
+    let riciObj = getRIDCIDfromAddress(address);
+    let rid = riciObj.rid;
+    let cid = riciObj.cid;
+    console.log(rid + " " + cid);
+    let uiCellElement = document.querySelector(`.cell[rid="${rid}"][cid="${cid}"]`);
+    console.log(uiCellElement);
+    uiCellElement.style.fontWeight = "bold";    
+})
+
+function getRIDCIDfromAddress(address) {
+    let cid = Number(address.charCodeAt(0)) - 65;
+    let rid = Number(address.slice(1)) - 1;
+    return {"rid":rid,"cid":cid};
 }
